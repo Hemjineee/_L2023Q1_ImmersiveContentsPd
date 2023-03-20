6cf979e319b22db57b19598001636b8f74b152cf
studio522 <lectures.202x@gmail.com>
2023-03-20 12:37:58 +0900

    0320

---
+++ Assets/Scripts/OnKeyDown_LoadScene.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnKeyDown_LoadScene : MonoBehaviour
+{
+    [Tooltip("æ¿¿ª πŸ≤‹ ≈∞∏¶ ±‚¿‘")]
+    public string Key;
+
+    [Tooltip("æ¿ ∆ƒ¿œ¿ª «“¥Á")]
+    public Object SceneFile;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKey(vKey))
+                {
+                    //your code here
+                    print(vKey);
+                    if (vKey.ToString().ToLower() == Key.ToString().ToLower())
+                    {
+                        print("got the key");
+                        SceneManager.LoadScene(SceneFile.name);
+                    }
+                }
+            }
+        }
+    }
+}
